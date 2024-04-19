5275eb2d240af2f2624ca7a97d2a078935b64893
mohamedelkhateeb <mohamedelkhteeb74@gmail.com>
2024-04-19 02:22:34 +0200

    mt

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const { Command } = require("commander");
+const program = new Command();
+
+program
+	.name("first-node-app")
+	.version("0.0.1")
+	.description("i am a Mohamed and this is my firts nodejs CLI app");
+
+  program.parse(process.argv);
